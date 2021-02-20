5042b7b9cbe6be8532501d0d038a0e6a1bb3d881
Asif Tamboli <aasif.aeez@gmail.com>
2021-02-20 19:54:12 +0530

    added few components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
-import Header from "./Header";
-
+import { useEffect, useState } from 'react';
+import CountButton from './CountButton/CountButton'
+import SearchBar from './SearchBar/SearchBar';
+import Spinner from './Spinner/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
-    const str1 = 'my own string'
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(list => {
+                setList(list);
+            })
+    }, [list]);
+
+    const hasList = list.length > 0;
     return (
-        <div className="container">
-            <Header />
-            <ul>
-                <li>item 1</li>
-                <li>{str1}</li>
-            </ul>
+        <div>
+            {
+                hasList ?
+                    <SearchBar searchList={list} />
+                    :
+                    <Spinner />
+            }
+
+            {/* <CountButton incrementBy="1" bgColor="sandybrown" txtColor="white"/>
+            <CountButton incrementBy="5" bgColor="green" txtColor="white"/> */}
         </div>
     );
 }
-// https://youtu.be/ABQLwlE8MUA?list=PLfuZzro3POlY8ZqHtHzZ994_xSsFC7mUA&t=4482
+// https://youtu.be/ABQLwlE8MUA?list=PLfuZzro3POlY8ZqHtHzZ994_xSsFC7mUA&t=7412
 export default App;
